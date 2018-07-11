0dc4c8e646a4d51c8ffd5153099ab0c3e6b77571
Logan Call <logancall.dev@gmail.com>
2018-07-11 12:00:06 -0600

    Move Screen, Header, Footer components to proper directory

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,9 +1,9 @@
 // @flow
 import * as React from 'react'
 //
-import { Screen } from '../../shared/components/Screen'
-import { Header } from '../../shared/components/Header'
-import { Footer } from '../../shared/components/Footer'
+import { Screen } from '../shared/components/Screen'
+import { Header } from '../shared/components/Header'
+import { Footer } from '../shared/components/Footer'
 
 function Home(props: any) {
   return <h1>Home Page</h1>
--- src/screens/NotFound/index.js
+++ src/screens/NotFound/index.js
@@ -1,9 +1,9 @@
 // @flow
 import * as React from 'react'
 //
-import { Screen } from '../../shared/components/Screen'
-import { Header } from '../../shared/components/Header'
-import { Footer } from '../../shared/components/Footer'
+import { Screen } from '../shared/components/Screen'
+import { Header } from '../shared/components/Header'
+import { Footer } from '../shared/components/Footer'
 
 function NotFound(props: any) {
   return <h1>404 Page</h1>
