c35e78a6a53d73ada9f7defd8a5590e23227a7ea
Logan Call <logancall.dev@gmail.com>
2018-09-19 10:07:35 -0600

    cleanup app.js, pull in error boundary from bit. fix mounting issue

--- src/App.js
+++ src/App.js
@@ -1,42 +1,14 @@
 import * as React from "react"
-import { Router, Route } from "react-static"
+import { Router } from "react-static"
 import Routes from "react-static-routes"
-import { ErrorBoundary } from "./shared/components/ErrorBoundary"
-import { Styles } from "./shared/components/Styles"
-import { Spring, animated } from "react-spring"
-
-const AnimatedStyles = animated(Styles)
-
-const RenderRoutes = ({ getComponentForPath }: any) => (
-  // The default renderer uses a catch all route to recieve the pathname
-  <Route
-    path="*"
-    render={(props) => {
-      // The pathname is used to retrieve the component for that path
-      let Comp = getComponentForPath(props.location.pathname)
-      // The component is rendered!
-      return (
-        <Spring
-          from={{ opacity: 0, transform: "scale(1.02)" }}
-          to={{ opacity: 1, transform: "scale(1)" }}
-        >
-          {(styles) => (
-            <AnimatedStyles style={styles}>
-              <Comp key={props.location.pathname} {...props} />
-            </AnimatedStyles>
-          )}
-        </Spring>
-      )
-    }}
-  />
-)
+import { ErrorBoundary } from "@bit/seamonster-studios.react.components.error-boundary"
 
 export default class App extends React.Component {
   render() {
     return (
       <Router>
         <ErrorBoundary>
-          <Routes component={RenderRoutes} />
+          <Routes />
         </ErrorBoundary>
       </Router>
     )
